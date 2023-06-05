fa8561e196c61c1119bddad7c8b9f8d5ba0090b3
Crpearce <Crpearce892@gmail.com>
2023-06-05 09:35:07 -0600

    display questions to dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,43 @@
-import './App.css';
+import { useEffect, useState } from 'react'
 
-function App() {
+import { Card, FormControl } from '@mui/material'
+
+import axios from 'axios'
+
+import './App.css'
+
+const App = () => {
+  const [allQuestions, setAllQuestions] = useState([])
+
+  const getTriviaData = async () => {
+    try {
+      const response = await axios.get(
+        'https://the-trivia-api.com/v2/questions/'
+      )
+      setAllQuestions(response.data)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    getTriviaData()
+  }, [])
+
+  console.log(allQuestions)
+
   return (
-    <div className="App">App
+    <div className='App'>
+      <h1>Colby's Trivia Game</h1>
+      {allQuestions.map(question => {
+        return (
+          <Card>
+            <h2>{question.question.text}</h2>
+          </Card>
+        )
+      })}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
