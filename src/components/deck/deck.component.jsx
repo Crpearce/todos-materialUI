c95df76a048ccffdc8a3b4ac9a002506f33c813d
Crpearce <Crpearce892@gmail.com>
2023-06-07 07:49:39 -0600

    psuedocode for random card functionality

--- src/components/deck/deck.component.jsx
+++ src/components/deck/deck.component.jsx
@@ -3,6 +3,10 @@ import TriviaCard from '../triviaCard/triviaCard.component'
 import './deck.styles.css'
 
 const Deck = ({ allQuestions }) => {
+
+  // create an initial function to generate a randmomized question from the allQuestions prop that is being passed down
+  // render the one random card to the screen, when an answer has been submit(button click), then splice that specific object out, 
+  // randomly generate another question for the user
   return (
     <div>
       {allQuestions.map((question) => {
