0c7347a360d56b94351735733df19fbaa98c27d8
Crpearce <Crpearce892@gmail.com>
2023-06-14 14:48:03 -0600

    cleanup

--- src/components/deck/deck.component.jsx
+++ src/components/deck/deck.component.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react'
 import TriviaCard from '../triviaCard/triviaCard.component'
 
 import './deck.styles.css'
@@ -7,26 +7,23 @@ const Deck = ({ allQuestions }) => {
   const [randomNumber, setRandomNumber] = useState()
   const [randomCard, setRandomCard] = useState()
   // create an initial function to generate a randmomized question from the allQuestions prop that is being passed down
-  // render the one random card to the screen, when an answer has been submit(button click), then splice that specific object out, 
+  // render the one random card to the screen, when an answer has been submit(button click), then splice that specific object out,
   // randomly generate another question for the user
-useEffect(() => {
+  useEffect(() => {
     setRandomNumber(Math.floor(Math.random() * 11))
     setRandomCard(allQuestions[randomNumber])
-}, [allQuestions]);
-//  console.log(allQuestions[randomNumber])
- console.log(randomCard)
+  }, [allQuestions])
+
   return (
     <div>
-     {randomCard &&
-
-       <TriviaCard
-       key={randomCard.id}
-       question={randomCard.question}
-       correctAnswer={randomCard.correctAnswer}
-       incorrectAnswers={randomCard.incorrectAnswers}
-       />
-      }
-        
+      {randomCard && (
+        <TriviaCard
+          key={randomCard.id}
+          question={randomCard.question}
+          correctAnswer={randomCard.correctAnswer}
+          incorrectAnswers={randomCard.incorrectAnswers}
+        />
+      )}
     </div>
   )
 }
