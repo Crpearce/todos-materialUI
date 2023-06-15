9a1fb6dd6becd2a73a1ab73520b9b501451652f9
Crpearce <Crpearce892@gmail.com>
2023-06-15 09:09:05 -0600

    add pseudocode, begin logic for answering

--- src/components/deck/deck.component.jsx
+++ src/components/deck/deck.component.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react'
+
 import TriviaCard from '../triviaCard/triviaCard.component'
 
 import './deck.styles.css'
@@ -6,13 +7,14 @@ import './deck.styles.css'
 const Deck = ({ allQuestions }) => {
   const [randomNumber, setRandomNumber] = useState()
   const [randomCard, setRandomCard] = useState()
-  // create an initial function to generate a randmomized question from the allQuestions prop that is being passed down
-  // render the one random card to the screen, when an answer has been submit(button click), then splice that specific object out,
-  // randomly generate another question for the user
+
   useEffect(() => {
     setRandomNumber(Math.floor(Math.random() * 11))
     setRandomCard(allQuestions[randomNumber])
   }, [allQuestions])
+
+  // need to now capture a users selected answer, and make sure that it is a match with the correct answer
+  // all answers need to be randomized, think of a function to change the order of each potential answer so they arent all the same
 
   return (
     <div>
